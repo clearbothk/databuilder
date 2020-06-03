88fef46db03e364868f9c2fc6b2ca7727228ca43
utkarsh867 <utkarsh867@gmail.com>
2020-06-03 23:30:35 +0530

    adds a zip file handler with checks for temporary files

--- OnZipUpload/Storage.py
+++ OnZipUpload/Storage.py
@@ -3,12 +3,16 @@ import uuid
 from azure.storage.blob import BlobServiceClient
 
 class Storage:
-  self._connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-  self.container_name = "unlabelled"
-
+  _connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+  container_name = "unlabelled"
+  
   def __init__(self):
     self.blob_service_client = BlobServiceClient.from_connection_string(self._connect_str)
-  
+    try:
+      container_client = blob_service_client.create_container(self.container_name)
+    except:
+      print("Did not create container. Container might already exist")
+
   def write(self, blob, file_ext):
     blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=f"{str(uuid.uuid4())}.{file_ext}")
     blob_client.upload_blob(blob)
--- OnZipUpload/__init__.py
+++ OnZipUpload/__init__.py
@@ -1,12 +1,16 @@
 import os
 import logging
 import tarfile
+import zipfile
+import re
+from zipfile import ZipFile
 import uuid
-from io import BufferedReader
+from io import BufferedReader, BytesIO
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
+from .storage import Storage
 
-def handle_tar(myblob: func.InputStream, blob_service_client, container_name):
+def handle_tar(myblob: func.InputStream, storage: Storage):
     with tarfile.open(fileobj=myblob, mode='r|*') as tar:
         tar.extractall()
         for f in tar.getmembers():
@@ -14,31 +18,32 @@ def handle_tar(myblob: func.InputStream, blob_service_client, container_name):
             file_ext = file_ext.lower()
             if file_ext == "jpeg" or file_ext == "jpg" or file_ext == "png":
                 with open(f.name, mode='rb') as img:
-                    blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{str(uuid.uuid4())}.{file_ext}")
-                    blob_client.upload_blob(img)
+                    storage.write(img, file_ext)
+
+def handle_zip(myblob: func.InputStream, storage: Storage):
+    regex = re.compile("^.*(\/)?\._.*")
+    zip_blob = BytesIO(myblob.read())
+    zip_file = ZipFile(zip_blob)
+    contents = zip_file.namelist()
+    contents_filtered = [item for item in contents if item not in list(filter(regex.search, contents))]
+    for f in contents_filtered:
+        print(f)
+        file_ext = f.split('.')[-1]
+        file_ext = file_ext.lower()
+
+        if file_ext == "jpeg" or file_ext == "jpg" or file_ext == "png":
+            with zip_file.open(f) as myfile:
+                storage.write(myfile, file_ext)
 
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
 
+    storage = Storage()
 
-    
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    container_name = "unlabelled"
-
-    try:
-        container_client = blob_service_client.create_container(container_name)
-    except:
-        print("Did not create container. Container might already exist")
-    
-    with tarfile.open(fileobj=myblob, mode='r|*') as tar:
-        out = tar.extractall()
-        for f in tar.getmembers():
-            file_ext = f.name.split('.')[-1]
-            file_ext = file_ext.lower()
-            if file_ext == "jpeg" or file_ext == "jpg" or file_ext == "png":
-                with open(f.name, mode='rb') as img:
-                    blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{str(uuid.uuid4())}.{file_ext}")
-                    blob_client.upload_blob(img)
-            
+    archive_extension = myblob.name.split('.')[-1]
+    if archive_extension == 'tar':
+        handle_tar(myblob, storage)
+    elif archive_extension == 'zip':
+        handle_zip(myblob, storage)
