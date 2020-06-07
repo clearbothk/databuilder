cd6b330985246c7a5e478f45cde14619e5d71f71
utkarsh867 <utkarsh867@gmail.com>
2020-06-07 15:35:59 +0530

    adds code for generatng upload token

---
+++ GenerateUploadToken/__init__.py
@@ -0,0 +1,34 @@
+import logging
+import os
+import uuid
+import datetime
+import json
+import azure.functions as func
+from azure.storage.blob import generate_container_sas, ContainerSasPermissions
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    file_name = str(uuid.uuid4()) + ".zip"
+    account_name = "vision4458150196"
+    container_name = "uploads"
+
+    _connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    access_key = os.getenv('STORAGE_ACCESS_KEY')
+
+    permissions = ContainerSasPermissions(read=True, write=True, delete=False, list=True)
+
+    now = datetime.datetime.utcnow()
+    end = now + datetime.timedelta(hours=1)
+
+    sas_token = generate_container_sas(account_name, container_name, account_key=access_key, permission=permissions, start=now, expiry=end)
+
+    print(sas_token)
+
+    responseData = json.dumps({
+        "token": sas_token,
+        "name": file_name
+    })
+    
+    return func.HttpResponse(mimetype="application/json", body=responseData)
