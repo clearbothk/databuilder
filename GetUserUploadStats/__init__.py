f4003bbe4b6891548d97849666286c981647b9c4
--replace-all <utkarsh867@gmail.com>
2021-03-01 15:30:04 +0800

    Return CSV from the endpoint

--- GetUserUploadStats/__init__.py
+++ GetUserUploadStats/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import json
+import csv
+import datetime
 import azure.functions as func
 from azure.cosmosdb.table.tableservice import TableService
 from azure.storage.blob import BlobServiceClient
@@ -12,6 +14,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     _connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     unlabelled_container = BlobServiceClient.from_connection_string(
         _connect_str).get_container_client("unlabelled")
+    labelling_container = BlobServiceClient.from_connection_string(
+        _connect_str).get_container_client("labelling")
     labelled_container = BlobServiceClient.from_connection_string(
         _connect_str).get_container_client("labelled")
     table_service = TableService(connection_string=_connect_str)
@@ -19,22 +23,37 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     unlabelled_blobs = list(map(lambda x: x["name"], list(
         unlabelled_container.list_blobs())))
+    labelling_blobs = list(map(lambda x: x["name"], list(
+        labelling_container.list_blobs())))
     labelled_blobs = list(map(lambda x: x["name"], list(
         labelled_container.list_blobs())))
 
     unlabelled_count = 0
     labelled_count = 0
     labelling_count = 0
+    other_count = 0
     total_uploads = 0
 
-    for entry in data_generator:
-        total_uploads += 1
-        filename = entry["RowKey"]
-        if filename in unlabelled_blobs:
-            unlabelled_count += 1
-        elif filename in labelled_blobs:
-            labelled_count += 1
-        else:
-            labelling_count += 1
+    email_mapping = {}
 
-    return json.dumps({"uploads": total_uploads, "unlabelled": unlabelled_count, "labelled": labelled_count, "labelling": labelling_count})
+    with open('report.csv', 'w') as csvfile:
+        for entry in data_generator:
+            total_uploads += 1
+            filename = entry["RowKey"]
+            timestamp = entry["Timestamp"]
+            if filename in unlabelled_blobs:
+                unlabelled_count += 1
+            elif filename in labelled_blobs:
+                labelled_count += 1
+            elif filename in labelling_blobs:
+                labelled_count += 1
+            else:
+                other_count += 1
+            current_email_count = email_mapping.get(entry["PartitionKey"], 0)
+            email_mapping[entry["PartitionKey"]] = current_email_count + 1
+            writer = csv.writer(csvfile)
+            writer.writerow(
+                [entry["PartitionKey"], timestamp.strftime("%c"), filename])
+    with open('report.csv', 'r') as report_file:
+        return report_file.read()
+    # json.dumps({"uploads": total_uploads, "unlabelled": unlabelled_count, "labelled": labelled_count, "labelling": labelling_count, "others": other_count, "emails": email_mapping})
